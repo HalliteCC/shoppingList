92f966dacb62c22f44e5a5e39a49d87bebf02f5e
HalliteCC <hallitecaie@gmail.com>
2023-04-18 17:56:47 -0300

    Button date

--- app/src/main/java/com/example/buylist/RegisterPlacesActivity.kt
+++ app/src/main/java/com/example/buylist/RegisterPlacesActivity.kt
@@ -1,21 +1,24 @@
 package com.example.buylist
 
+import android.app.DatePickerDialog
 import android.content.Intent
 import android.os.Bundle
 import android.view.View
+import android.widget.DatePicker
 import androidx.appcompat.app.AppCompatActivity
 import com.example.buylist.databinding.ActivityShoppingListBinding
+import java.util.*
 
-class RegisterPlacesActivity: AppCompatActivity() {
+class RegisterPlacesActivity : AppCompatActivity()  {
 
-private lateinit var binding: ActivityShoppingListBinding
+    private lateinit var binding: ActivityShoppingListBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         binding = ActivityShoppingListBinding.inflate(layoutInflater)
 
-        binding.buttonCreateList.setOnClickListener{
+        binding.buttonCreateList.setOnClickListener {
             TODO()
         }
 
@@ -23,5 +26,8 @@ private lateinit var binding: ActivityShoppingListBinding
         // Layout
         setContentView(binding.root)
     }
+
+
+
 
 }
--- app/src/main/java/com/example/buylist/RegisterProductAtivity.kt
+++ app/src/main/java/com/example/buylist/RegisterProductAtivity.kt
@@ -1,25 +1,59 @@
 package com.example.buylist
 
+import android.app.DatePickerDialog
 import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import android.widget.DatePicker
 import androidx.appcompat.app.AppCompatActivity
 import com.example.buylist.databinding.ActivityRegisterProductsBinding
+import java.text.SimpleDateFormat
+import java.util.*
 
 
-class RegisterProductAtivity: AppCompatActivity() {
+class RegisterProductAtivity : AppCompatActivity(), View.OnClickListener,
+    DatePickerDialog.OnDateSetListener {
+
     private lateinit var binding: ActivityRegisterProductsBinding
+    private val dateFormat = SimpleDateFormat("dd/MM/yyyy")
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         binding = ActivityRegisterProductsBinding.inflate(layoutInflater)
 
-
+        binding.buttonDate.setOnClickListener(this)
         binding.buttonSave.setOnClickListener {
             startActivity(Intent(applicationContext, RegisterPlacesActivity::class.java))
         }
 
         // Layout
         setContentView(binding.root)
+    }
+
+    override fun onClick(v: View) {
+        if (v.id == R.id.button_date) {
+            handleDate()
+        }else if (v.id == R.id.button_save){
+
+        }
+
+    }
+
+    override fun onDateSet(view: DatePicker?, year: Int, month: Int, dayOfMonth: Int) {
+        val calendar = Calendar.getInstance()
+        calendar.set(year, month, dayOfMonth)
+
+        val dueDate = dateFormat.format(calendar.time)
+        binding.buttonDate.text = dueDate
+    }
+
+    fun handleDate() {
+        val calendar = Calendar.getInstance()
+        val year = calendar.get(Calendar.YEAR)
+        val month = calendar.get(Calendar.MONTH)
+        val day = calendar.get(Calendar.DAY_OF_MONTH)
+        DatePickerDialog(this, this, year, month, day).show()
+
     }
 }
